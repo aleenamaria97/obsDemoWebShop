039c5b0000ecee70f5447de75edb4ce754b23bf9
aleena <aleenamariya97@gmail.com>
2021-11-18 20:58:09 +0530

    initial commit

--- src/main/java/com/demowebshop/constants/Constants.java
+++ src/main/java/com/demowebshop/constants/Constants.java
@@ -1,8 +1,8 @@
 package com.demowebshop.constants;
 
 public class Constants {
-    public static final String CONFIG_FILE="\\src\\main\\java\\com\\demowebshop\\automationcore";
-    public static final String EMAIL_ID="aleenamariajames19976@gmail.com";
-    public static final String PASS_WORD="#1Aleena97";
-    public static final String ToEMAIL_ID="aleenamariya97@gmail.com";
+    public static final String CONFIG_FILE = "\\src\\main\\java\\com\\demowebshop\\automationcore";
+    public static final String EMAIL_ID = "aleenamariajames19976@gmail.com";
+    public static final String PASS_WORD = "#1Aleena97";
+    public static final String ToEMAIL_ID = "aleenamariya97@gmail.com";
 }
